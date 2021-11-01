ef759bb5ba9b40342543c6f86cc125912d0b09b4
Zoey <xiaozhou@umail.ucsb.edu>
2021-11-01 16:06:56 -0700

    we added code to reaed from a static json file

--- src/pages/Practice.js
+++ src/pages/Practice.js
@@ -6,6 +6,7 @@ import Timer from "../utils/Timer";
 import { TextField } from "@mui/material";
 import CodeCard from "../utils/CodeCard";
 import WordCount from '../utils/WordCount'
+import CodeList from "../components/CodeList";
 
 export default function Practice() {
     const user = getUser();
@@ -15,6 +16,7 @@ export default function Practice() {
                 <div>
                     <Timer/>
                     <CodeCard/> 
+                    <CodeList/>
                     <WordCount/>
                 </div>
             </Container>
