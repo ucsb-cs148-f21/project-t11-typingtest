edf8e952f53a74b8a9045c35a1d0e1ee1b88649c
Jason Dunne <jasonrdunne@ucsb.edu>
2021-11-05 12:37:42 -0700

    added category routes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask, render_template, request, flash
 import mongoengine as db
-from functions import parseCodeFile, codesnippets, returnProblemFromIndex
+from functions import parseCodeFile, codesnippets, returnProblemFromIndex, returnProblemsFromLanguage, returnProblemsFromLanguageAndSkill
 database_name = "testdb"
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
@@ -15,9 +16,20 @@ if __name__ == "__main__":
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/codesnippet/<id>', methods=['GET'])
-def returnData(id):
+
+@app.route('/codesnippetID/<id>', methods=['GET'])
+def returnID(id):
     return returnProblemFromIndex(id)
+
+
+# codesnippet?language=Java&skill=2
+@app.route('/codesnippet/<language>/<skill>', methods=['GET'])
+def returnLanguageAndSkill(language, skill):
+    return returnProblemsFromLanguageAndSkill(language, skill)
+
+@app.route('/codesnippet/<language>', methods=['GET'])
+def returnLanguage(language):
+    return returnProblemsFromLanguage(language)
 
 @app.errorhandler(404)
 def not_found(e):
