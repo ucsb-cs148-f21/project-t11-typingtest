26a25b52632a18239f8d8a3dde6ab2f0c02564a4
Jason Dunne <jasonrdunne@ucsb.edu>
2021-11-19 12:30:35 -0800

    added numbers to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -12,22 +12,32 @@ const Doggo = styled.img`
   height: auto;
 `;
 
+state = {
+  easyProblems: 0,
+  mediumProblems: 0,
+  hardProblems: 0
+};
+
 export default function Profile() {
   const user = getUser();
 
   var fetchURL = window.location.href
-  fetchURL = fetchURL.replace("profile", "profile/")
+  fetchURL += "/"
   fetchURL += user.id
   console.log(fetchURL)
   fetch(fetchURL)
     .then(response => response.json())
-
+    .then(data => {
+      console.log(data);
+      console.log(this.state.text)
+      this.setState({easyProblems: JSON.stringify(data.Easy), mediumProblems: JSON.stringify(data.Medium), hardProblems: JSON.stringify(data.Hard)})
+    })
   return (
     <Layout user={user}>
       <Container>
         <h1>Welcome back {user.givenName}!</h1>
         <div>
-          Number of easy problems completed: 
+          Number of easy problems completed: this.state.
         </div>
           Number of medium problems completed: 
         <div>
@@ -35,7 +45,6 @@ export default function Profile() {
         </div>
           List of problems completed: 
         <br />
-        <Doggo src={DoggoImg} />
       </Container>
     </Layout>
   );
