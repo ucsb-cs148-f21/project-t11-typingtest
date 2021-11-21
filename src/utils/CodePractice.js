9a445b1e67cd6a0fdf48ba40410299ff0a458691
Hirish Chandrasekaran <hirish@ucsb.edu>
2021-11-20 16:14:22 -0800

    added stringify

--- src/utils/CodePractice.js
+++ src/utils/CodePractice.js
@@ -6,7 +6,9 @@ export default function CodePractice(match) {
         console.log(match.match.params.id);
 	}, []);
 
-	const [item, setItem] = useState({code:"a"});
+	const [item, setItem] = useState({code:"a"}); 
+	const [code, setCode] = useState({code:"a"}); 
+	const name = 'Josh Perez';
     
 	const fetchItem = async () => {
 		const fetchItem = await fetch(`http://127.0.0.1:5000/codesnippet/Java`
@@ -14,13 +16,19 @@ export default function CodePractice(match) {
         const item = await fetchItem.json();
         setItem(item);
         console.log(item);
+		console.log(item[0]);
+		console.log(item[0].code);
+		console.log(item[match.match.params.id].code);
+		console.log(typeof item[0].code)
+		const code = item[match.match.params.id].code;
+		setCode(code)
+		console.log(code);
+		console.log(typeof code)
 	}
 
 	return (
-		<div>
-			<h1>hello</h1>
-            <div>{item[match.match.params.id].code}</div>
-		</div>
-	)
+		<pre>
+			{JSON.stringify(code, null, 2)}
+		</pre>
+	);
 }
-
