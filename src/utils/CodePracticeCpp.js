88803493e2186f42e0324859c00e896a8840cf59
Zoey <xiaozhou@umail.ucsb.edu>
2021-12-02 18:31:16 -0800

    added textID

--- src/utils/CodePracticeCpp.js
+++ src/utils/CodePracticeCpp.js
@@ -19,27 +19,27 @@ export default function CodePracticeCpp(match) {
         );
         const item = await fetchItem.json();
         setItem(item);
-        console.log(item);
-		console.log(item[0]);
-		console.log(item[0].code);
+        //console.log(item);
+		//console.log(item[0]);
+		//console.log(item[0].code);
 		//console.log(item[match.match.params._id].code);
 		//console.log(typeof item[0].code)
 		//const code = item[match.match.params._id].code;
 		//setCode(code)
 		//console.log(code);
-		console.log(typeof code)
-
+		//console.log(typeof code)
+		console.log(match.match.params._id);
 		for(var i = 0; i < item.length; i++) {
 			var obj = item[i];
 			if (obj._id  === match.match.params._id)
 			{
 				console.log(obj);
-				setCode(obj.code)
+				setCode(obj.code);
 				console.log(obj.code);
-				setID(obj._id)
+				setID(obj._id);
 			}
 		}
-		console.log(_id)
+		console.log(_id);
 	}
 	if(code !== ""){
 	
